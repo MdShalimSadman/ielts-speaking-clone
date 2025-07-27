c3b1579ca192c58948f66b96c356feb41863f97c
MdShalimSadman <shalimsadmandipro3@gmail.com>
2025-07-27 15:23:28 +0600

    feat(localization): localization handled
    - installed the package next intl
    - handled with i18n routing

---
+++ src/app/[lang]/layout.tsx
@@ -0,0 +1,19 @@
+import { NextIntlClientProvider} from "next-intl";
+import "../globals.css";
+
+export default async function RootLayout({
+  children,
+  params,
+}: Readonly<{
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+}>) {
+  const { locale } = await params;
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
