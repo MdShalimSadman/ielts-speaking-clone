b564486d6769fcab1448ccbe71fedaddefd65c17
MdShalimSadman <shalimsadmandipro3@gmail.com>
2025-07-28 23:36:30 +0600

    feat(carousel): button for carousel installed from shadcn

---
+++ .next/server/chunks/ssr/messages_bn_json_bc99e9f3._.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+"[project]/messages/bn.json (json)": ((__turbopack_context__) => {
+
+__turbopack_context__.v(JSON.parse("{\"HomePage\":{\"ratings\":\"(81.8% শিক্ষার্থী কোর্স শেষে ৫ রেটিং দিয়েছেন)\"}}"));}),
+
+};
---
+++ .next/static/chunks/src_app_[lang]_page_tsx_b991c109._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_4785603f._.js",
+  "static/chunks/src_ebbf4174._.js"
+],
+    source: "dynamic"
+});
