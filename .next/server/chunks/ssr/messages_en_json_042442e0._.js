94a252310c2c23977c8ea177ef708424468ea718
MdShalimSadman <shalimsadmandipro3@gmail.com>
2025-07-29 02:46:15 +0600

    feat(right-section): right section built successfully

--- .next/server/chunks/ssr/messages_en_json_042442e0._.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-
-"[project]/messages/en.json (json)": ((__turbopack_context__) => {
-
-__turbopack_context__.v(JSON.parse("{\"HomePage\":{\"ratings\":\"(81.8% শিক্ষার্থী কোর্স শেষে ৫ রেটিং দিয়েছেন)\"}}"));}),
-
-};
