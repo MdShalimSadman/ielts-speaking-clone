a63f4f256ac7e78150f26677109e7c6179de358d
MdShalimSadman <shalimsadmandipro3@gmail.com>
2025-07-29 14:28:40 +0600

    fix(type): built with the type instead of name in course detail

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"R3lauSJKmzKwiuWWdmzezfuuFA658L9y66ZpX+aDroE=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
