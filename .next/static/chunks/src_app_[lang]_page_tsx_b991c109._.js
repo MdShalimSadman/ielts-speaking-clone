19da8e8a6c97c6de7e4f31c0c43c79c58c2a2d97
MdShalimSadman <shalimsadmandipro3@gmail.com>
2025-07-29 12:48:01 +0600

    feat(metadata): built with metadata

--- .next/static/chunks/src_app_[lang]_page_tsx_b991c109._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_6928d3a3._.js",
-  "static/chunks/node_modules_49167ae3._.js"
-],
-    source: "dynamic"
-});
